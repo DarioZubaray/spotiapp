f601d569d58cbce7d19f1c24b37458c758eb4be0
Dario Jose Zubaray <dzubaray@gmail.com>
2018-10-09 08:47:04 -0300

    implementando la llamada al servicio de spotify con un token temporal

---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor(private http: HttpClient) {
+    console.log('Spotify service ready!');
+  };
+
+  getRequest() {
+    const headers = new HttpHeaders ({
+      'Authorization':'Bearer BQAOoKATZeIAl8JluC_o7Tii-YuYNcFrwjPu7DNSXG025fllRuFDzl-zCkLQPR1Xf5bm7gOLIN1bGILP23_rssfEMS9PH-zdrhZZPT0TwIlnmmXPdVyqrWUHNr8owJV4BcqhIyeqRYPNgcjlw7wR3amr7AGnAl9Tsw'
+    });
+    let url = 'https://api.spotify.com/v1/browse/new-releases';
+    this.http.get(url, { headers }).subscribe( data => {
+      console.log(data);
+    });
+  }
+}
