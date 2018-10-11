b5bcf7678184ab4f44aa72f4774c46c064e15b2e
Dario Jose Zubaray <dzubaray@gmail.com>
2018-10-11 09:03:31 -0300

    completando la lista de top-tracks del artista con vista previa

--- src/app/components/artista/artista.component.ts
+++ src/app/components/artista/artista.component.ts
@@ -10,6 +10,7 @@ import { SpotifyService } from '../../services/spotify.service';
 export class ArtistaComponent {
 
   artista: any = {};
+  topTracks: any[] = [];
   loading: boolean;
 
   constructor(private route: ActivatedRoute, private spotiSer: SpotifyService) {
@@ -17,14 +18,22 @@ export class ArtistaComponent {
     this.route.params.subscribe( params => {
       console.log(params['id']);
       this.getArtista(params['id']);
+      this.getTopTracks(params['id']);
     });
   }
 
-  getArtista( id:string) {
+  getArtista( id:string ) {
     this.spotiSer.getArtista(id).subscribe( data => {
       console.log(data);
       this.artista = data;
       this.loading = false;
+    });
+  }
+
+  getTopTracks( id:string ) {
+    this.spotiSer.getTopTracks(id).subscribe( data => {
+      console.log(data);
+      this.topTracks = data;
     });
   }
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -35,4 +35,9 @@ export class SpotifyService {
   getArtista( id: string) {
     return this.getQuery(`artists/${id}`);
   }
+
+  getTopTracks( id: string) {
+    return this.getQuery(`artists/${id}/top-tracks?country=ES`)
+                .pipe( map( data => data['tracks'] ));
+  }
 }
