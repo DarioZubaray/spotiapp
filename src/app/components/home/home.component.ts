adc403d65b6b9182ddb29be662a9e5cb9c598060
Dario Jose Zubaray <dzubaray@gmail.com>
2018-10-11 09:30:56 -0300

    capturando los errores en el home con un alert

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
@@ -6,21 +6,27 @@ import { SpotifyService } from '../../services/spotify.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent{
 
   nuevasCanciones: any[] = [];
   loading: boolean;
+  error: boolean;
+  mensajeError: string;
 
   constructor(private spotiSer: SpotifyService){
     this.loading = true;
-    this.spotiSer.getRequest().subscribe( (data : any) => {
+    this.error = false;
+
+    this.spotiSer.getRequest().subscribe( ( data : any ) => {
       console.log(data);
       this.nuevasCanciones = data;
       this.loading = false;
+    }, ( errorServicio: any ) => {
+      this.loading = false;
+      this.error = true;
+      this.mensajeError = errorServicio.error.error.message;
     });
   };
 
-  ngOnInit() {
-  }
 
 }
